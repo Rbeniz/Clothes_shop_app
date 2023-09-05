d175e5484f40e6fa103d20a2440a9a1eacc8dfe2
Zi ~ <zi@MacBook-Pro-de-Zi.local>
2023-09-05 23:29:41 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,328 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import '/Carousselle/Carousselle.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter TikTok',
+      debugShowCheckedModeBanner: false,
+      home: Carousselle(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HomePage(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/images/tiktok_add.png',
+              height: 25,
+            ),
+            label: 'Add',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.comment_outlined),
+            label: 'Comment',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: 'Profil',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: const Color(0xFF141518),
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+//////////////////
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  final List tiktokItems = [
+    "assets/videos/video_1.mp4",
+    "assets/videos/video_2.mp4",
+    "assets/videos/video_3.mp4",
+    "assets/videos/video_4.mp4",
+    "assets/videos/video_5.mp4",
+    "assets/videos/video_6.mp4",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: double.infinity,
+        scrollDirection: Axis.vertical,
+        viewportFraction: 1.0,
+      ),
+      items: tiktokItems.map((index) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+              color: const Color(0xFF141518),
+              child: Stack(
+                children: [Image.asset(index), const PostContent()],
+              ),
+            );
+          },
+        );
+      }).toList(),
+    );
+  }
+}
+
+////////////////
+///
+/* class VideoWidget extends StatefulWidget {
+  const VideoWidget({Key? key, required this.videoUrl}) : super(key: key);
+  final String videoUrl;
+
+  @override
+  _VideoWidgetState createState() => _VideoWidgetState(this.videoUrl);
+}
+
+class _VideoWidgetState extends State<VideoWidget> {
+  late VideoPlayerController _controller;
+  final String videoUrl;
+  _VideoWidgetState(this.videoUrl);
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset(videoUrl)
+      ..initialize().then((_) {
+        _controller.setLooping(true);
+        _controller.play();
+        setState(() {});
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return VideoPlayer(_controller);
+  }
+}
+ */
+class PostContent extends StatelessWidget {
+  const PostContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 100,
+          padding: const EdgeInsets.only(top: 40),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text(
+                'Following',
+                style: TextStyle(
+                  color: Colors.white54,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(width: 20),
+              Text(
+                'For you',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        '@extremesports_95',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        'Goein full send in Squaw Valley. #snow @snowboarding # extremesports #sendit #winter',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: const [
+                          Icon(
+                            Icons.music_note,
+                            color: Colors.white,
+                            size: 15,
+                          ),
+                          SizedBox(width: 5),
+                          Text(
+                            'Original Sound - extremesports_95',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                width: 80,
+                padding: const EdgeInsets.only(bottom: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    SizedBox(
+                      height: 80,
+                      child: Stack(
+                        alignment: AlignmentDirectional.bottomCenter,
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(bottom: 10),
+                            child: const CircleAvatar(
+                              radius: 25,
+                              backgroundImage:
+                                  AssetImage('assets/images/photo-5.jpeg'),
+                            ),
+                          ),
+                          Container(
+                            padding: const EdgeInsets.all(2),
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: const Icon(
+                              Icons.add,
+                              color: Colors.white,
+                              size: 15,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    SizedBox(
+                      height: 80,
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.favorite,
+                            color: Colors.white.withOpacity(0.85),
+                            size: 45,
+                          ),
+                          const Text(
+                            '25.0K',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 80,
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.comment,
+                            color: Colors.white.withOpacity(0.85),
+                            size: 45,
+                          ),
+                          const Text(
+                            '156',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 80,
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.share,
+                            color: Colors.white.withOpacity(0.85),
+                            size: 45,
+                          ),
+                          const Text(
+                            '123',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
